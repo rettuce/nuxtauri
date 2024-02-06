b28943fc8671f15fece85d6668a39ccad965e392
you tanaka <you@rettuce.com>
2024-02-07 05:39:48 +0900

    Tauri v2 beta update
    Tauri 2.0.0-beta にアップデート
    参照 https://beta.tauri.app/guides/frontend/nuxt/

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,16 +1,16 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use tauri::Manager;
+// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+#[tauri::command]
+fn greet(name: &str) -> String {
+    format!("Hello, {}! You've been greeted from Rust!", name)
+}
 
 fn main() {
-  tauri::Builder::default()
-    .setup(|app| {
-        let main_window =app.get_window("main").unwrap();
-        // main_window.eval("window.location.replace('https://google.com')").unwrap();
-        main_window.eval(&format!("window.location.replace('http://localhost:{}')", "3000")).unwrap();
-        Ok(())
-    })
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    tauri::Builder::default()
+        .plugin(tauri_plugin_shell::init())
+        .invoke_handler(tauri::generate_handler![greet])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
